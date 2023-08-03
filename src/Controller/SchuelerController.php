b2eba30a64b7c1a98e4063a41679cbb37756ad55
mmcbbq <m.martinez@bbq.de>
2023-08-03 17:11:42 +0200

    besser spät als nie

--- src/Controller/SchuelerController.php
+++ src/Controller/SchuelerController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 
 
+use App\Entity\Schueler;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SchuelerController
+class SchuelerController extends AbstractController
 {
     #[Route('/')]
     public function schueler() :Response
@@ -15,9 +18,45 @@ class SchuelerController
         return new Response('Hallo Welt');
     }
 
-    #[Route('/show/{slug}')]
-    public function show(int $slug ) :Response
+//    #[Route('/show/{id}')]
+//    public function show(int $id, EntityManagerInterface $entityManager) :Response
+//    {
+//        $repository = $entityManager->getRepository(Schueler::class);
+//        $schueler = $repository->find($id);
+//
+//        return new Response('Hallo '.$schueler->getNachname());
+//    }
+
+
+    #[Route('/createschueler')]
+    public function createschueler (EntityManagerInterface $entityManager) :Response
     {
-        return new Response('show datensatz '.$slug);
+        $neuerSchueler = new Schueler();
+        $neuerSchueler->setNachname('Doe');
+        $neuerSchueler->setEmail('Doe@joe.de');
+        $neuerSchueler->setTelefonNummer('0123456');
+        $neuerSchueler->setKommentar('Ist ein Joe');
+
+        $entityManager->persist($neuerSchueler);
+        $entityManager->flush();
+
+
+        return new Response('Schüler '.$neuerSchueler->getNachname() .' hinzugefügt');
+}
+
+    #[Route('/schuelertest/{id}')]
+    public function schuelertest (Schueler $schueler)
+    {
+        return new Response('Hallo' .$schueler->getNachname());
     }
+    #[Route('update/{id}')]
+    public function update(Schueler $schueler, EntityManagerInterface $entityManager)
+    {
+        $schueler->setNachname('karl');
+        $entityManager->persist($schueler);
+        $entityManager->flush();
+        return new Response('dein name ist jetzt' .$schueler->getNachname());
+
+    }
+
 }
