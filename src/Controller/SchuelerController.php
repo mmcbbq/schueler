846193556d0cfe03b7cca092c00d3c74e220d965
Erik Berndt <erikberndt@gmx.net>
2023-08-03 15:27:37 +0200

    crud schueler controller

--- src/Controller/SchuelerController.php
+++ src/Controller/SchuelerController.php
@@ -4,20 +4,67 @@ namespace App\Controller;
 
 
 
+use App\Entity\Schueler;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SchuelerController
+class SchuelerController extends AbstractController
 {
-    #[Route('/')]
-    public function schueler() :Response
-    {
-        return new Response('Hallo Welt');
-    }
+  #[Route('/')]
+  public function schueler(): Response
+  {
+    return new Response('Hallo Welt');
+  }
 
-    #[Route('/show/{slug}')]
-    public function show(int $slug ) :Response
-    {
-        return new Response('show datensatz '.$slug);
-    }
+  //info CREATE
+  #[Route('/createSchueler')]
+  public function createSchueler(EntityManagerInterface $entityManager):Response
+  {
+    $schueler = new Schueler();
+    $schueler->setNachname('samson');
+    $schueler->setTelefonNummer('4122');
+    $schueler->setEmail('s@b.s');
+    $schueler->setKommentar('sesamstrasse');
+    $entityManager->persist($schueler);
+    $entityManager->flush();
+    return new Response('schueler created');
+  }
+
+  //info READ
+//  #[Route('/show/{slug}')]
+//  public function show(int $slug, EntityManagerInterface $entityManager): Response
+//  {
+//    $repo = $entityManager->getRepository(Schueler::class);
+//    $schueler = $repo->find($slug);
+//    return new Response('show datensatz ' . $schueler->getNachname());
+//  }
+
+  //info READ
+  #[Route('/schuelertest/{id}')]
+  public function readSchueler(Schueler $schueler):Response
+  {
+    return new Response('Hallo '.$schueler->getNachname());
+  }
+
+  //info UPDATE
+  #[Route('/updateSchueler/{id}')]
+  public function updateSchueler(Schueler $schueler, EntityManagerInterface $entityManager):Response
+  {
+    $old = $schueler->getNachname();
+    $schueler->setNachname('erik');
+    $entityManager->persist($schueler);
+    $entityManager->flush();
+    return new Response($old . ' heisst jetzt ' . $schueler->getNachname());
+  }
+
+  //info DELETE
+  #[Route('/deleteSchueler/{id}')]
+  public function deleteSchueler(Schueler $schueler, EntityManagerInterface $entityManager):Response
+  {
+    $entityManager->remove($schueler);
+    $entityManager->flush();
+    return new Response('Schueler gelöscht');
+  }
 }
