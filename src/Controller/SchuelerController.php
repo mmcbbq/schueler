32f66744c82c2409139806f0eaf2234c5d276d90
Fil <concatofilippo94@gmail.com>
2023-08-04 10:37:00 +0200

    filippo

--- src/Controller/SchuelerController.php
+++ src/Controller/SchuelerController.php
@@ -3,21 +3,66 @@
 namespace App\Controller;
 
 
-
+use App\Entity\Schueler;
+use App\Repository\SchuelerRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-class SchuelerController
+class SchuelerController extends AbstractController
 {
+
+
     #[Route('/')]
-    public function schueler() :Response
+    public function schueler(): Response
     {
         return new Response('Hallo Welt');
     }
 
-    #[Route('/show/{slug}')]
-    public function show(int $slug ) :Response
+    #[Route('schueler/show/{id}')]
+    public function showschueler(Schueler $schueler) :Response
     {
-        return new Response('show datensatz '.$slug);
+        return $this->render('schueler/show.html.twig',[
+            'schueler'=> $schueler
+        ]);
+    }
+    #[Route('schueler/test/{id}')]
+    public function schuelerTest (Schueler $schueler): Response
+    {
+        return new Response('Hello ' . $schueler->getNachname());
+    }
+
+    #[Route('/schueler/create')]
+    public function create(EntityManagerInterface $em): Response
+    {
+        $schueler = new Schueler();
+        $schueler->setNachname('Karl');
+        $schueler->setEmail('aaa@aaa.aaa');
+        $schueler->setTelefonNummer('0123456789');
+        $schueler->setKommentar('Cooler Typ');
+
+
+        $em->persist($schueler);
+        $em->flush();
+
+        return new Response('Schueler Created!');
+    }
+
+    #[Route('schueler/update/{id}')]
+    public function update(Schueler $schueler, EntityManagerInterface $em): Response
+    {
+        $schueler->setNachname('Karl');
+        $em->persist($schueler);
+        $em->flush();
+
+        return new Response('Dein name ist jetzt ' . $schueler->getNachname());
+    }
+
+    #[Route('schueler/delete/{id}')]
+    public function delete(Schueler $schueler, EntityManagerInterface $em): Response
+    {
+        $em->remove($schueler);
+        $em->flush();
+        return new Response($schueler->getNachname() . ' Schueler wird gelöscht');
     }
 }
