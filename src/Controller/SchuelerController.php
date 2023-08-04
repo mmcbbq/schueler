8f1aa479967f02d57612bb318f1b553b7d323922
Fil <concatofilippo94@gmail.com>
2023-08-05 01:58:11 +0200

    filippo - prepared manage() to be dynamic

--- src/Controller/SchuelerController.php
+++ src/Controller/SchuelerController.php
@@ -18,12 +18,20 @@ class SchuelerController extends AbstractController
     #[Route('/')]
     public function schueler()
     {
-        return new Response('Hallo Welt');
+        return $this->render('schueler/home.html.twig');
+    }
+
+//    #[Route('schueler/show/{id}')]
+    private function show(Schueler $schueler) :Response
+    {
+        return $this->render('schueler/show.html.twig', [
+            'schueler' => $schueler
+        ]);
     }
 
 
-    #[Route('schueler/showall')]
-    public function showAll(EntityManagerInterface $em): Response
+//    #[Route('schueler/show/all')]
+    private function showAll(EntityManagerInterface $em): Response
     {
         $repository = $em->getRepository(Schueler::class);
         $schuelers = $repository->findAll();
@@ -37,45 +45,46 @@ class SchuelerController extends AbstractController
     {
         switch ($fn) {
             case 'create':
-                $schueler = new Schueler();
-                $schueler->setVorname('Karl');
-                $schueler->setNachname('Karlenson');
-                $schueler->setEmail('aaa@aaa.aaa');
-                $schueler->setTelefonNummer('0123456789');
-                $schueler->setKommentar('Cooler Typ');
+                // still static. $data needs to get value from somewhere
+                $data = [
+                    'vorname' => 'Karl',
+                    'nachname' =>'Karlenson',
+                    'email' => 'aaa@aaa.aaa',
+                    'telefonNummer' => '0123456789',
+                    'kommentar' => 'Cooler Typ'
+                ];
 
+                $schueler = $this->create($data);
                 $em->persist($schueler);
                 $em->flush();
 
-                $schueler->getId();
-                return $this->render('schueler/show.html.twig', [
-                    'schueler' => $schueler
-                ]);
+                return $this->show($schueler);
+
             case 'read':
-                $schueler = $schuelerRepository->find($id);
+                if ($id === 'all'){
+                    return $this->showAll($em);
+                } else {
+                    $schueler = $schuelerRepository->find($id);
 
-                if (!$schueler) {
-                    return new Response('Schueler not found');
+                    if (!$schueler) {
+                        return new Response('Schueler not found');
+                    }
+
+                    return $this->show($schueler);
                 }
-
-                return $this->render('schueler/show.html.twig', [
-                    'schueler' => $schueler
-                ]);
-
             case 'update':
                 $schueler = $schuelerRepository->find($id);
 
                 if (!$schueler) {
                     return new Response('Schueler not found');
                 }
-// still static
+                // still static. $data needs to get value from somewhere
                 $data = ['vorname' => 'Yooooo'];
-                $this->updateSchuelerAttributes($schueler, $data);
+                $this->update($schueler, $data);
                 $em->persist($schueler);
                 $em->flush();
-                return $this->render('schueler/show.html.twig', [
-                    'schueler' => $schueler,
-                ]);
+
+                return $this->show($schueler);
 
             case 'delete':
                 $schueler = $schuelerRepository->find($id);
@@ -86,7 +95,7 @@ class SchuelerController extends AbstractController
                 $em->remove($schueler);
                 $em->flush();
 
-                return new Response($schueler->getNachname() . ' Schueler has been removed');
+                return $this->showAll($em);
 
             default:
                 return new Response('Invalid operation');
@@ -94,26 +103,29 @@ class SchuelerController extends AbstractController
     }
 
     // will need this when it will be dynamic
-    private function updateSchuelerAttributes(Schueler $schueler, array $info)
+    private function update(Schueler $schueler, array $data)
     {
-        foreach ($info as $attribute => $value) {
+        foreach ($data as $attribute => $value) {
             //if the attribute is "nachname", the generated setter method name will be "setNachname".
             $setter = 'set' . ucfirst($attribute);
             //call setter method
             $schueler->$setter($value);
-
         }
     }
 
+    // will need this when it will be dynamic
+    private function create(array $data): Schueler
+    {
+        $schueler = new Schueler();
+        foreach ($data as $attribute => $value) {
+            //loop to every assignable key
+            $setter = 'set' . ucfirst($attribute);
+            //call setter method
+            $schueler->$setter($value);
+        }
+        return $schueler;
+    }
 
-//    #[Route('schueler/show/{id}')]
-//    public function show(Schueler $schueler) :Response
-//    {
-//        return $this->render('schueler/show.html.twig',[
-//            'schueler'=> $schueler
-//        ]);
-//    }
-//
 //    #[Route('/schueler/create')]
 //    public function create(EntityManagerInterface $em): Response
 //    {
@@ -148,7 +160,7 @@ class SchuelerController extends AbstractController
 //        return new Response($schueler->getNachname() . ' Schueler wird gelöscht');
 //    }
 //
-    //    #[Route('schueler/test/{id}')]
+//    #[Route('schueler/test/{id}')]
 //    public function schuelerTest (Schueler $schueler): Response
 //    {
 //        return new Response('Hello ' . $schueler->getNachname());
