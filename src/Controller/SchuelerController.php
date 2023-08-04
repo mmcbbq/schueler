b8af6e8dd104318a56e5d1cbd1607dedbf7e3b03
Petra Tsesmetzi <p_tsesmetzi@gmx.de>
2023-08-04 09:12:47 +0200

    crud operation für Schüler hinzugefügt

--- src/Controller/SchuelerController.php
+++ src/Controller/SchuelerController.php
@@ -4,20 +4,67 @@ namespace App\Controller;
 
 
 
+use App\Entity\Schueler;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SchuelerController
+class SchuelerController extends AbstractController
 {
+
+
     #[Route('/')]
     public function schueler() :Response
     {
         return new Response('Hallo Welt');
     }
+    //READ
+//    #[Route('/show/{id}')]
+//    public function show(int $id,EntityManagerInterface $entityManager ):Response
+//    {
+//        $repository=$entityManager->getRepository(Schueler::class);
+//        $schueler=$repository->find($id);
+//        return new Response('show datensatz '.$schueler->getNachname());
+//    }
 
-    #[Route('/show/{slug}')]
-    public function show(int $slug ) :Response
+//kurzeschreibweise für das auslesen id ist wichtig
+    #[Route('/shuelertest/{id}')]
+    public function shuelertest(Schueler $schueler ):Response
     {
-        return new Response('show datensatz '.$slug);
+        return new Response('shuelertest '.$schueler->getKommentar());
     }
+
+//CREATE
+    #[Route('/createSchueler/')]
+    public function createSchueler(EntityManagerInterface $entityManager):Response{
+        //$entityManager-ist dafür verantwortlich das objekte aus der Datenbank gespeichert und rausgelsene werden.
+        $neuerSchueler=new Schueler();
+        $neuerSchueler->setNachname('Feuerstein');
+        $neuerSchueler->setEmail('f-stein@gmx.de');
+        $neuerSchueler->setTelefonNummer('73737373737');
+        $neuerSchueler->setKommentar('hello you');
+        //dd($neuerSchueler);//dump and die beendet und gibt  das object aus
+        $entityManager->persist($neuerSchueler); //persist erzählt der doctrine kümmer dich mal drum
+        $entityManager->flush(); //doctrone schaut nach allen fügt das Objekt hinzu
+        return new Response($neuerSchueler->getNachname());
+    }
+
+    //UPDATE
+    #[Route('update/{id}')]
+    public function update(Schueler $schueler,EntityManagerInterface $entityManager){
+
+        $schueler->setNachname('otto');
+        $entityManager->persist($schueler);
+        $entityManager->flush();//flush ist wie excute und führt aus
+        return new Response('name ist jetzt: '.$schueler->getNachname());
+
+    }
+    #[Route('delete/{id}')]
+    public function delete(Schueler $schueler,EntityManagerInterface $entityManager):Response{
+        $entityManager->remove($schueler);
+        $entityManager->flush();
+        return new Response('ist gelöscht');
+    }
+
 }
